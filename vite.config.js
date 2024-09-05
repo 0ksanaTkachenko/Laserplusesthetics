0c75e052e4d2338ac0053c82b6953a057542437e
Oksana <ksenia.tkachenko.ua@gmail.com>
2024-09-05 15:42:22 +0700

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
-  base: '/Laserplusesthetics/', // Убедитесь, что это имя вашего репозитория
+  // base: '/Laserplusesthetics/',
   plugins: [react()],
   resolve: {
     alias: {
