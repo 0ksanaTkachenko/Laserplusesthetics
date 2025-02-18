1358b1bf5b47715fa81262d0f61b54490fe81eb3
Oksana <ksenia.tkachenko.ua@gmail.com>
2025-02-18 21:00:48 +0700

    fix goldButton file path

--- src/pages/home/appointment/appointment.jsx
+++ src/pages/home/appointment/appointment.jsx
@@ -2,7 +2,7 @@
 import './appointment.css';
 import ModalAppointment from '../../../components/modalAppointment/modalAppointment';
 import avocadoGirl from '@assets/images/HomePage/appoitment-section/avocado-girl.png';
-import GoldButton from '@components/GoldButton/GoldButton'
+import GoldButton from '@components/goldButton/goldButton'
 import { useState } from 'react';
 
 const Appointment = () => {
