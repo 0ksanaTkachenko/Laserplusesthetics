6dd81a9df492c226dc0d02fa988c7a99ad54ca9e
Oksana <ksenia.tkachenko.ua@gmail.com>
2025-02-18 20:57:29 +0700

    Remove all cached files to fix case sensitivity issues

--- src/pages/FAQ/FAQ.jsx
+++
@@ -1,9 +0,0 @@
-import './FAQ.css';
-
-const FaqPage = () => {
-
-    return <div className='about'>FAQ page in progress...</div>
-
-}
-
-export default FaqPage
--- src/pages/Results/Results.jsx
+++
@@ -1,9 +0,0 @@
-import './Results.css';
-
-const ResultsPage = () => {
-
-    return <div className='about'>Results page in progress...</div>
-
-}
-
-export default ResultsPage
