bdd73c1503bda431b1b072b678b23fb8c630db6e
Oksana <ksenia.tkachenko.ua@gmail.com>
2025-02-18 14:09:54 +0700

    fix home page path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '@components/Header/DesktopHeader/Header';
-import Footer from '@components/Footer/Footer';
-import HomePage from './pages/home/Home';
+// import Footer from '@components/Footer/Footer';
+import HomePage from './pages/home/home';
 import AboutPage from './pages/about/about';
 import ServicesPage from '@pages/Services/Services'
 import ResultsPage from '@pages/Results/Results'
