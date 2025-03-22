f09720c0e2c174b93dfb6152ade5143873eadced
Oksana <ksenia.tkachenko.ua@gmail.com>
2025-03-21 20:08:04 -0400

    Fix Vite base for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
 
 export default defineConfig({
-  base: '/Laserplusesthetics/',
+  // base: '/Laserplusesthetics/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
